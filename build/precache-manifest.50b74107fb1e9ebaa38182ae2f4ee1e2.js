ab2ac6db3d2c0e57aac88e5e79be199d30e0e56a
qubit88 <qubertec@gmail.com>
2019-05-18 10:51:18 +0300

    remove build from gitignore for netlify

---
+++ build/precache-manifest.50b74107fb1e9ebaa38182ae2f4ee1e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0155adb17cca4b86895c723e175e4e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4fe31373896ce866161",
+    "url": "/static/css/main.6e93df97.chunk.css"
+  },
+  {
+    "revision": "ed23474de0b9538895e6",
+    "url": "/static/js/2.aeaa37c2.chunk.js"
+  },
+  {
+    "revision": "e4fe31373896ce866161",
+    "url": "/static/js/main.3b6e76c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
